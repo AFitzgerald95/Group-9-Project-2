598d9f8963ae9f64ca3866c53781e3c2e07089c5
Victor Hernandez <141363547+BlackisDay@users.noreply.github.com>
2023-12-07 19:08:37 -0500

    Added route for handlebars, beginning handlebars

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const session = require('express-session')
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({})
 //end Require
 
 
@@ -19,6 +21,8 @@ const SC = {
     })
 }
 app.use(session(SC))
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
