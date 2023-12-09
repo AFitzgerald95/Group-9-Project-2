2f9eeb57847d90a4cc945e729c0aa1c08b93cc74
Lujoffily <luna.joffily@gmail.com>
2023-12-09 10:46:27 -0500

    axios finish

--- public/js/axios.js
+++ public/js/axios.js
@@ -4,34 +4,43 @@ let searchBtn = document.getElementById("searchBtn");
 let paramsBtn = document.getElementById("paramsBtn");
 
 const bookURL = () => {
-    const bookKey = "AIzaSyCWP2wkkvDpkqCGnXd_GS6WWc-tI6DKZtM"
+    const bookKey = "AIzaSyCWP2wkkvDpkqCGnXd_GS6WWc-tI6DKZtM";
+    let poster = "";
+    let descript = "";
     axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}=all&key=${bookKey}`)
     .then(response => {
         console.log(response)
+        const bookID = response.data[0].id
+    }).then(data => {
+        poster += data.poster;
+        descript += data.plot_overview
+    })
+    .catch(err => {
+        console.log(err)
     }).catch(err => {
         console.log(err)
-    });
-}
-
-const videoURL = () => {
-    const videoKey = "ohYP4vnMNvPPW1O0egOBVQfqKwvQqDOTKITGb4cI"
-    axios.get(`https://api.watchmode.com/v1/search/?apiKey=${videoKey}&search_field=name&search_value=${params}`)
-    .then( response => {
-        console.log(response);
-    }).catch(err => {
-        console.log(err);
     })
 }
 
-// const sendURL = () => {
-//     axios.get('https://reqres.in/api/register', {
-//         email: 'eve.holt@reqres.in',
-//         // password: 'pistol'
-//     }).then(response => {
-//         console.log(response);
-//     }).catch(err => {
-//         console.log(err, err.response);
-//     })
-// }
+const videoURL = () => {
+    const videoKey = "ohYP4vnMNvPPW1O0egOBVQfqKwvQqDOTKITGb4cI";
+    let poster = "";
+    let descript = "";
+    axios.get(`https://api.watchmode.com/v1/search/?apiKey=${videoKey}&search_field=name&search_value=${params}`)
+    .then( response => {
+        console.log(response)
+        const itemID = response.data[0].id
+        axios.get(`https://api.watchmode.com/v1/title/${itemID}/details/?apiKey=${videoKey}&append_to_response=sources`)
+            .then(data => {
+        poster += data.poster
+        descript += data.plot_overview
+    }).catch(err => {
+        console.log(err)
+    })
+}).catch(err => {
+    console.log(err)
+})
+}
+
 searchBtn.addEventListener('click', bookURL);
 paramsBtn.addEventListener('click', videoURL);
