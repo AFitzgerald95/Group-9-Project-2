b761b167bca1495e58001e003af497dbb0873533
Luna Joffily <luna.joffily@gmail.com>
2023-12-15 12:41:08 -0500

    Worked on cards

--- public/js/axios.js
+++ public/js/axios.js
@@ -1,6 +1,7 @@
 let searchBtn = document.getElementById("searchBtn");
 let paramsBtn = document.getElementById("paramsBtn");
 
+
 const bookURL = () => {
     let search = document.getElementById('searchInput').value;
     const bookKey = "AIzaSyCWP2wkkvDpkqCGnXd_GS6WWc-tI6DKZtM";
@@ -12,7 +13,7 @@ const bookURL = () => {
             console.log(response.data.items)
             const bookID = book.id
             poster += book.volumeInfo.imageLinks.thumbnail;
-            descript += book.volumeInfo.description
+            descript += book.volumeInfo.description;
         })
 
 }
@@ -29,7 +30,6 @@ const videoURL = () => {
     let us_rating = "";
     axios.get(`https://api.watchmode.com/v1/search/?apiKey=${videoKey}&search_field=name&search_value=${params}`)
         .then(async response => {
-            console.log(response)
             var videoResults = response.data.title_results;
             for(var i = 0; i<videoResults.length; i++){
                 await axios.get(`https://api.watchmode.com/v1/title/${videoResults[i].id}/details/?apiKey=${videoKey}&append_to_response=sources`)
@@ -37,11 +37,6 @@ const videoURL = () => {
                     console.log(data)
                     poster += data.poster
                     descript += data.plot_overview
-                    genres += data.genre_names[10]
-                    OT += data.original_title;
-                    OL += data.original_language;
-                    title = data.title;
-                    us_rating = data.usrating;
                 }).catch(err => {
                     console.log(err)
                 })}
