320200b0b88e761cff368b260a54af9708d38633
Luna Joffily <luna.joffily@gmail.com>
2024-01-04 19:19:34 -0500

    fixed issue with video cards

--- public/js/axios.js
+++ public/js/axios.js
@@ -15,7 +15,6 @@ const bookURL = () => {
 
             const newCardEl = document.createElement('div');
             newCardEl.classList.add('bookCard');
-        
 
             const bookImgUrl = book.volumeInfo.imageLinks.smallThumbnail
             if (bookImgUrl) {
@@ -63,4 +62,67 @@ const bookURL = () => {
         });
     }
 
+    const videoURL = () => {
+        const videoKey = "ohYP4vnMNvPPW1O0egOBVQfqKwvQqDOTKITGb4cI";
+        let params = document.getElementById('paramsInput').value;
+        axios.get(`https://api.watchmode.com/v1/search/?apiKey=${videoKey}&search_field=name&search_value=${params}`)
+            .then(async response => {
+                var videoResults = response.data.title_results;
+                for(var i = 0; i<videoResults.length; i++){
+                    await axios.get(`https://api.watchmode.com/v1/title/${videoResults[i].id}/details/?apiKey=${videoKey}&append_to_response=sources`)
+                    .then(data => {
+                        const videoEl = document.querySelector('.videoContainer');
+    
+                        var video = data;
+                        console.log(video);
+
+                        const videoCardEl = document.createElement('div');
+                        videoCardEl.classList.add('videoCard');
+
+                        const videoImg = video.data.poster;
+                        if (videoImg) {
+                            const videoImgEl = document.createElement('img');
+                            videoImgEl.classList.add('image');
+                            videoImgEl.src = videoImg;
+                            videoCardEl.appendChild(videoImgEl);
+                        }
+
+                        const videoName = video.data.original_title;
+                        if (videoName) {
+                            const videoNameEl = document.createElement('h4');
+                            videoNameEl.classList.add('videoName');
+                            videoNameEl.textContent = videoName;
+                            videoCardEl.appendChild(videoNameEl);
+                        }
+
+                        const videoDescrition = video.data.plot_overview;
+                        if (videoDescrition) {
+                            const videoDescritionEl = document.createElement('p');
+                            videoDescritionEl.classList.add('description');
+                            videoDescritionEl.textContent = videoDescrition;
+                            videoCardEl.appendChild(videoDescritionEl);
+                        }
+
+                        const videoTrailer = video.data.trailer;
+                        if (videoTrailer) {
+                            const videoTrailerEl = document.createElement('button');
+                            videoTrailerEl.classList.add('link');
+                            if (videoEl) {
+                                videoEl.appendChild(videoTrailerEl);
+                                videoTrailerEl.addEventListener('click', () =>
+                                window.location.href = videoTrailer
+                            )
+                            } 
+                            videoCardEl.appendChild(videoTrailerEl);
+                            videoTrailerEl.textContent = 'Watch Trailer';
+                        }
+
+                        videoEl.appendChild(videoCardEl);
+                    }).catch(err => {
+                        console.log(err)
+                    })}
+            })
+    }
+
 searchBtn.addEventListener('click', bookURL);
+paramsBtn.addEventListener('click', videoURL);
